a1d13bbb889453931f185d777ad301fab8309cc5
hongsign <hongsign@gmail.com>
2016-12-12 12:44:42 -0800

    Add files via upload

---
+++ Stack.swift
@@ -0,0 +1,30 @@
+//
+//  Stack.swift
+//  Dev
+//
+//  Created by YU HONG on 2016-12-05.
+//  Copyright © 2016 homesoft. All rights reserved.
+//
+
+import Foundation
+
+class Stack<Element> {
+    var items = [Element]()
+    
+    func push (item: Element) {
+        items.append(item)
+    }
+    
+    func pop() -> Element {
+        return items.removeLast()
+    }
+    
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+    
+    var count: Int {
+        return items.count
+    }
+    
+}
